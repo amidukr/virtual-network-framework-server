4422322f2e9c20ed2b09c27cac28f5a80e33c7ea
Dmytro Brazhnyk <dmitry@brazhnik.org>
2017-06-11 12:24:19 +0300

    Configuring gradle multi-project build, and creating jetty project

---
+++ vnf-core/src/main/java/org/vnf/server/core/commonservice/CommonServiceHandlersConfiguration.java
@@ -0,0 +1,32 @@
+package org.vnf.server.core.commonservice;
+
+import org.vnf.server.core.commandprocessor.ConnectionLostHandler;
+import org.vnf.server.core.commandprocessor.InvokeHandler;
+import org.vnf.server.core.commandprocessor.ServiceHandlersConfiguration;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by qik on 6/4/2017.
+ */
+public class CommonServiceHandlersConfiguration implements ServiceHandlersConfiguration {
+
+    protected InvokeHandler createLoginHandler() {
+        return new LoginCommandHandler();
+    }
+
+    protected InvokeHandler createPingHandler() {
+        return new PingCommandHandler();
+    }
+
+    @Override
+    public Collection<InvokeHandler> getInvokeHandlers() {
+        return Arrays.asList(createLoginHandler(), createPingHandler());
+    }
+
+    @Override
+    public Collection<ConnectionLostHandler> getConnectionLostHandlers() {
+        return null;
+    }
+}
---
+++ vnf-jetty/src/main/java/org/vnf/server/ServerLauncher.java
@@ -0,0 +1,10 @@
+package org.vnf.server;
+
+/**
+ * Created by qik on 6/11/2017.
+ */
+public class ServerLauncher {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
